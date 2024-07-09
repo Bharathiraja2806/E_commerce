25cfb1cd719b4511a3e949ed8e01126c99f92bbe
Bharathiraja2806 <bharathiraja9708@gmail.com>
2024-07-09 09:03:02 +0530

    database added

--- E_commercepro/shopkart/admin.py
+++ E_commercepro/shopkart/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class categoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'image', 'description')
+
+admin.site.register(Product)
+admin.site.register(category, categoryAdmin)
+
