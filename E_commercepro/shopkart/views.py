86474a6a81255b462a94d2b836bac0d9f02952e4
Bharathiraja2806 <bharathiraja9708@gmail.com>
2024-07-11 09:11:28 +0530

    created collections view

--- E_commercepro/shopkart/views.py
+++ E_commercepro/shopkart/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from django.contrib import messages
 
 def home(request):
     return render(request, 'shop/index.html')
@@ -10,3 +11,12 @@ def register(request):
 def collections(request):
     Category = category.objects.filter(status=0)    
     return render(request, 'shop/collections.html',{'category':Category})
+
+def collectionsview(request, name):
+    if(category.objects.filter(name=name , status=0)) :
+        products = Product.objects.filter(category__name = name) 
+        return render(request, 'shop/products/index.html',{'products':products})
+    else:
+        messages.warning(request, "NO Such Products Found")
+        return redirect('collections')
+
